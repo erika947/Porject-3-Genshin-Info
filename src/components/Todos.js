28ccfdb2dfe90f920b531a42708100f5cd751a06
Erika Rivera <rivera.erika07@gmail.com>
2022-09-08 12:59:17 -0400

    deploy to heroku

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -43,7 +43,7 @@ export default function Todos() {
                 </div>
 
             </div>
-            {/* <Create /> */}
+            {/* <Create /> need to try and fix*/}
         </div>
     )
 
--- src/services/todos-api.js
+++ src/services/todos-api.js
@@ -1,43 +1,43 @@
 import axios from 'axios'
 
 export const getTodos = () => {
-    const URL = 'http://localhost:3001/todos'
+    const URL = 'https://genshin-api-erika.herokuapp.com/todos'
     const response = axios.get(URL)
     return response
 }
 export const getTodo = (id) => {
-    const URL = `http://localhost:3001/todos/${id}`
+    const URL = `https://genshin-api-erika.herokuapp.com/todos/${id}`
     const response = axios.get(URL)
     return response
 }
 
 export const deleteTodo = (id) => {
-    const URL = `http://localhost:3001/todos/${id}`
+    const URL = `https://genshin-api-erika.herokuapp.com/todos/${id}`
     const response = axios.delete(URL)
     return response
 }
 
 export const editTodo = (id, updatedTodo) => {
-    const URL = `http://localhost:3001/todos/${id}`
+    const URL = `https://genshin-api-erika.herokuapp.com/todos/${id}`
     const response = axios.put(URL, updatedTodo)
     return response
 }
 
 export const createTodo = (todo) => {
-    const URL = 'http://localhost:3001/todos'
+    const URL = 'https://genshin-api-erika.herokuapp.com/todos/'
     const response = axios.post(URL, todo)
     return response
 }
 
 export const getWeapons = (weapons) => {
-    const URL = 'http://localhost:3001/todos/weapons'
+    const URL = 'https://genshin-api-erika.herokuapp.com/todos/weapons'
     const response = axios.get(URL, weapons)
     return response
 }
 
 export const getWeapon = (id) => {
     console.log('getweapon')
-    const URL = `http://localhost:3001/weapons/${id}`
+    const URL = `https://genshin-api-erika.herokuapp.com/weapons/${id}`
     console.log(URL)
     const response = axios.get(URL)
     return response
